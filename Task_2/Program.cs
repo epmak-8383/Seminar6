4b1ca9555ec8c68a97edbeeab794d1eb40f4f216
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-18 23:35:19 +0300

    second

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите k1: ");
+double k1 = int.Parse(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = int.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = int.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = int.Parse(Console.ReadLine());
+
+double x = (b2-b1)/(k1-k2);
+double y1 = (k1*x +b1);
+// double y2 = (k2*x +b2);
+Console.WriteLine(x);
+Console.WriteLine(y1);
